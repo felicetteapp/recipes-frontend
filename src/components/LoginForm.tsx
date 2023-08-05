bb7356848bfbe70ca51291ced9715a729efbaed7
Facundo Leites <leitesfacundo@gmail.com>
2023-08-05 13:23:26 -0300

    refactor: login and register

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,4 +1,5 @@
 import {
+  Alert,
   Button,
   Card,
   CardActions,
@@ -12,14 +13,18 @@ import {
   InputAdornment,
   InputLabel,
   OutlinedInput,
+  Stack,
   TextField,
+  Typography,
+  styled,
 } from "@mui/material";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import { memo, useCallback, useState } from "react";
+import { memo, useCallback, useEffect, useState } from "react";
 import { firebaseApp } from "../services/firebase";
 import { useTranslation } from "react-i18next";
 import { RegisterUserModal } from "./RegisterUserModal";
-
+import felicetteRecipeLogo from "../assets/felicette_recipes_logo.png";
+import { useMatch, useNavigate } from "react-router-dom";
 const LoginFormBase = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -27,24 +32,54 @@ const LoginFormBase = () => {
   const [sending, setSending] = useState(false);
   const [registerModal, setRegisterModal] = useState(false);
   const { t } = useTranslation();
+  const [error, setError] = useState(false);
+  const navigate = useNavigate();
+
+  const isRegisterUserRoute = useMatch("login/register");
 
   const handleOnSubmit = useCallback(
     async (e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault();
       try {
+        setError(false);
         setSending(true);
         await signInWithEmailAndPassword(getAuth(firebaseApp), email, password);
       } catch (e) {
-        window.alert("ops!");
+        // TODO handle each error type
+        setError(true);
         setSending(false);
       }
     },
     [email, password]
   );
 
+  useEffect(() => {
+    if (isRegisterUserRoute) {
+      setRegisterModal(true);
+    } else {
+      setRegisterModal(false);
+    }
+  }, [isRegisterUserRoute]);
+
   return (
     <>
       <Container>
+        <Stack
+          direction="row"
+          spacing={1}
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          <LogoImg src={felicetteRecipeLogo} />
+          <Stack>
+            <Typography variant="h5" color="primary">
+              {t("felicette.felicette")}
+            </Typography>
+            <Typography variant="h6" color="secondary">
+              {t("felicette.recipes")}
+            </Typography>
+          </Stack>
+        </Stack>
         <form onSubmit={handleOnSubmit}>
           <Card>
             <CardHeader title={t("actions.login")} />
@@ -86,13 +121,20 @@ const LoginFormBase = () => {
                 />
               </FormControl>
             </CardContent>
+            {error && (
+              <CardContent>
+                <Alert severity="error" onClose={() => setError(false)}>
+                  {t("common.genericError")}
+                </Alert>
+              </CardContent>
+            )}
             <CardActions>
               {sending ? (
                 <Button fullWidth disabled>
                   <CircularProgress />
                 </Button>
               ) : (
-                <Button type="submit" fullWidth>
+                <Button type="submit" variant="contained" fullWidth>
                   {t("actions.login")}
                 </Button>
               )}
@@ -104,17 +146,19 @@ const LoginFormBase = () => {
           color="secondary"
           fullWidth
           sx={{ marginTop: 2 }}
-          onClick={() => setRegisterModal(true)}
+          onClick={() => navigate("register")}
         >
           {t("actions.createUser")}
         </Button>
       </Container>
-      <RegisterUserModal
-        open={registerModal}
-        onClose={() => setRegisterModal(false)}
-      />
+      <RegisterUserModal open={registerModal} onClose={() => navigate(-1)} />
     </>
   );
 };
 
 export const LoginForm = memo(LoginFormBase);
+
+const LogoImg = styled("img")`
+  height: auto;
+  max-width: min(100%, 100px);
+`;
